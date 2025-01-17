eace631916c88d3eba17de7025e364c0179b71dd
nna1205 <ngnhatanh1205.com>
2025-01-17 18:13:39 +0700

    Add featured product section components and styling margin to page, change section folder name

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -4,9 +4,10 @@ import {
   PageDocument,
 } from "@/graphql/types/graphql";
 import { notFound } from "next/navigation";
-import HeroSection from "@/components/sections/HeroSectionRecord/HeroSection";
-import TestimonialSection from "@/components/sections/TestimonialSectionRecord/TestimonialSection";
-import ServiceSection from "@/components/sections/ServiceSectionRecord/ServiceSection";
+import HeroSection from "@/components/Sections/HeroSectionRecord/HeroSection";
+import TestimonialSection from "@/components/Sections/TestimonialSectionRecord/TestimonialSection";
+import ServiceSection from "@/components/Sections/ServiceSectionRecord/ServiceSection";
+import FeaturedProductSection from "@/components/Sections/FeaturedProductSectionRecord/FeaturedProductSection";
 
 export async function generateStaticParams() {
   const { allPages } = await request(PageStaticParamsDocument, {});
@@ -25,21 +26,37 @@ export default async function Page({
     notFound();
   }
   return (
-    <>
-      {pageData.page?.sections.map((section) => {
-        switch (section.__typename) {
-          case "HeroSectionRecord":
-            return <HeroSection fragment={section} key={section.__typename} />;
-          case "TestimonialSectionRecord":
-            return (
-              <TestimonialSection fragment={section} key={section.__typename} />
-            );
-          case "ServicesSectionRecord":
-            return <ServiceSection fragment={section} key={section.__typename} />
-          default:
-            return null;
-        }
-      })}
-    </>
+    <div className="w-screen min-h-screen py-10 my-20 gap-8 px-4 lg:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-40 row-start-2 items-center sm:items-start">
+        {pageData.page?.sections.map((section) => {
+          switch (section.__typename) {
+            case "HeroSectionRecord":
+              return (
+                <HeroSection fragment={section} key={section.__typename} />
+              );
+            case "TestimonialSectionRecord":
+              return (
+                <TestimonialSection
+                  fragment={section}
+                  key={section.__typename}
+                />
+              );
+            case "ServicesSectionRecord":
+              return (
+                <ServiceSection fragment={section} key={section.__typename} />
+              );
+            case "FeaturedProductSectionRecord":
+              return (
+                <FeaturedProductSection
+                  fragment={section}
+                  key={section.__typename}
+                />
+              );
+            default:
+              return null;
+          }
+        })}
+      </main>
+    </div>
   );
 }
