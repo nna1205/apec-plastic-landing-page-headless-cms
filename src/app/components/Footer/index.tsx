aa52a7329e7a7eec6cd1d510d3c3be2ec80adbfb
nna1205 <ngnhatanh1205.com>
2025-01-17 18:04:58 +0700

    Add new header and  footer components

---
+++ src/app/components/Footer/index.tsx
@@ -0,0 +1,72 @@
+import { LayoutRecord } from "@/graphql/types/graphql";
+import Link from "next/link";
+import Image from "next/image";
+import Icon, { IconName } from "@/components/LucideIcon";
+
+type FooterProps = LayoutRecord;
+
+const Footer: React.FC<{ data: FooterProps }> = ({ data }) => {
+  return (
+    <footer className="flex flex-col justify-center items-center">
+      <div className="w-full flex flex-col lg:flex-row bg-green-400">
+        <div className="w-full lg:w-1/2 flex flex-col text-white gap-3 lg:gap-6 p-6 lg:p-20">
+          <div className="flex flex-col justify-start gap-2 mr-auto">
+            <span className="text-3xl lg:text-5xl font-black">
+              {data.companyName}
+            </span>
+            <span className="text-lg lg:text-2xl font-light">
+              {data.companyFullname}
+            </span>
+            <div className="flex lg:justify-start items-start lg:items-center gap-2 lg:gap-6 text-md lg:text-xl">
+              {data.contacts.map((item) => (
+                <div key={item.id} className="flex items-center gap-1">
+                  <Icon name={item.icon as IconName} size={24} color="white" />
+                  <span className="text-white">{item.value}</span>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="flex items-center gap-3">
+            {data.socialContacts.map((item) => {
+              return (
+                <div
+                  key={item.id}
+                  className="text-xl border-2 rounded-xl px-3 py-1 lg:text-md lg:px-6 lg:py-2"
+                >
+                  <Link href={item.url}>{item.label}</Link>
+                </div>
+              );
+            })}
+          </div>
+          <div className="flex flex-col justify-start text-wrap text-base gap-1 lg:gap-3">
+            {data.addresses.map((item) => (
+              <p
+                key={item.id}
+                className="flex flex-wrap items-center opacity-80"
+              >
+                {item.label}
+                <span className="font-bold opacity-100">
+                  {item.addressDetail}
+                </span>
+              </p>
+            ))}
+          </div>
+          {/* <NewsLetterSignup /> */}
+        </div>
+        <div className="w-full lg:w-1/2 relative h-[240px] lg:h-[640px]">
+          <Image
+            src={data.footerImage.url}
+            alt="Apec Plastic"
+            fill
+            sizes="(min-width: 1024px) 50%, 100%"
+          />
+        </div>
+      </div>
+      <div className="w-full bg-green-800 flex justify-center items-center text-white text-sm py-2 lg:text-2xl lg:py-4">
+        {data.copyrightStatement}
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/app/components/Header/index.tsx
@@ -0,0 +1,50 @@
+import { LayoutRecord } from "@/graphql/types/graphql";
+import Link from "next/link";
+import Image from "next/image";
+import Icon, { IconName } from "@/components/LucideIcon";
+import Navigation from "./Navigation";
+import SearchModal from "@/components/SearchModal";
+
+type HeaderProps = LayoutRecord;
+
+const Header: React.FC<{ data: HeaderProps }> = ({ data }) => {
+  return (
+    <header className="fixed top-0 left-0 w-screen z-50">
+      <div className="w-full bg-green-800 flex items-center gap-6 py-3 px-6 lg:px-20 text-white text-sm lg:text-lg">
+        {data.contacts.map((item) => (
+          <a key={item.id} className="flex justify-center items-center">
+            <Icon
+              name={item.icon as IconName}
+              className="h-4 w-4 lg:w-6 lg:h-6"
+            />
+            <span className="ml-2 font-bold">{item.value}</span>
+          </a>
+        ))}
+      </div>
+      <div className="bg-white w-full px-6 lg:px-20 py-1 lg:py-3 relative flex lg:justify-between items-center shadow-sm">
+        <Link href="/" className="flex justify-center items-center gap-3">
+          <div className="relative w-10 h-10 lg:w-16 lg:h-16">
+            <Image
+              src={data.logo.url as string}
+              alt="logo"
+              fill
+              sizes="(min-width: 1024px) 50%, 100%"
+            />
+          </div>
+          <div className="hidden lg:flex lg:w-full flex-col items-start">
+            <span className="line-clamp-1 text-2xl font-bold">
+              {data.companyName}
+            </span>
+            <span className="line-clamp-1 text-sm font-light">
+              {data.companyFullname}
+            </span>
+          </div>
+        </Link>
+        <Navigation data={data.navigation} />
+        <SearchModal />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
