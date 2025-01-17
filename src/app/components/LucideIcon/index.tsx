82bd1674b7c4bcd9dba336585fd1dbf173d31084
nna1205 <ngnhatanh1205.com>
2025-01-17 18:02:47 +0700

    Export type for Icon name

--- src/app/components/LucideIcon/index.tsx
+++ src/app/components/LucideIcon/index.tsx
@@ -2,7 +2,9 @@ import dynamic from "next/dynamic";
 import { LucideProps } from "lucide-react";
 import dynamicIconImports from "lucide-react/dynamicIconImports";
 
-export interface IconProps extends LucideProps {
+export type IconName = keyof typeof dynamicIconImports;
+
+interface IconProps extends LucideProps {
   name: keyof typeof dynamicIconImports;
 }
 
