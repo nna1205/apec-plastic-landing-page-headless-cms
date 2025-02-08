afbaa2b7a8d2f5cdea1d559bef6f6f42e56e93b4
nna1205 <ngnhatanh1205.com>
2025-02-08 23:45:37 +0700

    Implement integration and inital setup with next-intl package

---
+++ src/i18n/request.ts
@@ -0,0 +1,18 @@
+import {getRequestConfig} from 'next-intl/server';
+import {routing} from './routing';
+import { type SiteLocale } from "@/graphql/types/graphql";
+ 
+export default getRequestConfig(async ({requestLocale}) => {
+  // This typically corresponds to the `[locale]` segment
+  let locale = await requestLocale as SiteLocale;
+ 
+  // Ensure that a valid locale is used
+  if (!locale || !routing.locales.includes(locale as SiteLocale)) {
+    locale = routing.defaultLocale;
+  }
+ 
+  return {
+    locale,
+    messages: (await import(`../../messages/${locale}.json`)).default
+  };
+});
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,53 +1,9 @@
-import { match } from '@formatjs/intl-localematcher';
-import Negotiator from 'negotiator';
-import { type NextRequest, NextResponse } from 'next/server';
-import getAvailableLocales, { getFallbackLocale } from '@/i18n/setting';
-import type { SiteLocale } from '@/graphql/types/graphql';
-
-async function getLocale(
-  request: Request,
-  locales: SiteLocale[],
-): Promise<string> {
-  const fallbackLng = await getFallbackLocale();
-  const headers = new Headers(request.headers);
-  const acceptLanguage = headers.get('accept-language');
-  if (acceptLanguage) {
-    headers.set('accept-language', acceptLanguage.replaceAll('_', '-'));
-  }
-
-  const headersObject = Object.fromEntries(headers.entries());
-  const languages = new Negotiator({ headers: headersObject }).languages();
-  return match(languages, locales, fallbackLng);
-}
-
-export async function middleware(request: NextRequest) {
-  // Check if there is any supported locale in the pathname
-  const pathname = request.nextUrl.pathname;
-  const locales = await getAvailableLocales();
-
-  const pathnameIsMissingLocale = locales.every(
-    (locale) =>
-      !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`,
-  );
-
-  //go to home in browser language if pathname & locale is missing
-  if (pathname === '/') {
-    const locale = await getLocale(request, locales);
-    return NextResponse.redirect(new URL(`/${locale}/home`, request.url));
-  }
-
-  //go to pathname in browser language if locale is missing but pathname is set
-  if (pathnameIsMissingLocale) {
-    const locale = await getLocale(request, locales);
-
-    // e.g. incoming request is /products
-    // The new URL is now /en/products
-    return NextResponse.redirect(
-      new URL(`/${locale}/${pathname}`, request.url),
-    );
-  }
-}
-
+import createMiddleware from 'next-intl/middleware';
+import {routing} from './i18n/routing';
+ 
+export default createMiddleware(routing);
+ 
 export const config = {
-  matcher: ['/((?!.*\\.|_next|api\\/).*)'],
+  // Match only internationalized pathnames
+  matcher: ['/', '/(vi|en)/:path*', '/((?!_next|_vercel|.*\\..*).*)']
 };
