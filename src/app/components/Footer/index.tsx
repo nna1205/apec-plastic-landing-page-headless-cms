62edece33ecf645c3f906fea42abffffeda2f3af
nna1205 <ngnhatanh1205.com>
2025-02-14 11:43:39 +0700

    Change UI design of footer component

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -1,29 +1,29 @@
 import { LayoutQuery } from "@/graphql/types/graphql";
 import Link from "next/link";
-import Image from "next/image";
-import Icon, { IconName } from "@/components/LucideIcon";
+import ContactForm from "@/components/ContactForm";
+import { useTranslations } from "next-intl";
 
 type FooterProps = LayoutQuery;
 
 const Footer: React.FC<{ data: FooterProps }> = ({ data }) => {
+  const t = useTranslations();
   return (
-    <footer className="flex flex-col justify-center items-center">
-      <div className="w-full flex flex-col lg:flex-row bg-green-400">
-        <div className="w-full lg:w-1/2 flex flex-col text-white gap-3 lg:gap-6 p-6 lg:p-20">
-          <div className="flex flex-col justify-start gap-2 mr-auto">
-            <span className="text-3xl lg:text-5xl font-black">
-              {data.brandDetail?.companyName}
-            </span>
-            <span className="text-lg lg:text-2xl font-light">
-              {data.brandDetail?.companyFullname}
-            </span>
-            <div className="flex lg:justify-start items-start lg:items-center gap-2 lg:gap-6 text-md lg:text-xl">
-              {data.brandDetail?.contacts.map((item) => (
-                <div key={item.id} className="flex items-center gap-1">
-                  <Icon name={item.icon as IconName} size={24} color="white" />
-                  <span className="text-white">{item.value}</span>
-                </div>
-              ))}
+    <footer className="flex flex-col justify-center items-center border-t border-gray-200">
+      <div className="w-full flex flex-col lg:flex-row bg-white px-10">
+        <div className="w-full lg:w-1/2 flex flex-col gap-3 lg:gap-6 p-6 lg:p-10">
+          <div className="flex justify-center items-center gap-3">
+            <img
+              src={data.brandDetail?.logo.url as string}
+              alt="logo"
+              className="w-16 h-16 lg:w-24 lg:h-24"
+            />
+            <div className="flex flex-col justify-start gap-2 mr-auto">
+              <span className="text-green-800 text-3xl lg:text-5xl font-black">
+                {data.brandDetail?.companyName}
+              </span>
+              <span className="text-lg lg:text-2xl font-light">
+                {data.brandDetail?.companyFullname}
+              </span>
             </div>
           </div>
           <div className="flex items-center gap-3">
@@ -38,7 +38,7 @@ const Footer: React.FC<{ data: FooterProps }> = ({ data }) => {
               );
             })}
           </div>
-          <div className="flex flex-col justify-start text-wrap text-base gap-1 lg:gap-3">
+          <div className="flex flex-col justify-start text-wrap text-base gap-1">
             {data.brandDetail?.addresses.map((item) => (
               <p
                 key={item.id}
@@ -51,18 +51,17 @@ const Footer: React.FC<{ data: FooterProps }> = ({ data }) => {
               </p>
             ))}
           </div>
-          {/* <NewsLetterSignup /> */}
         </div>
-        <div className="w-full lg:w-1/2 relative h-[240px] lg:h-[640px]">
-          <Image
-            src={data.rootLayout?.footerImage.url as string}
-            alt="Apec Plastic"
-            fill
-            sizes="(min-width: 1024px) 50%, 100%"
-          />
+        <div className="w-full lg:w-1/2 p-6 lg:p-10">
+          <div className="">
+            <p className="text-sm lg:text-base text-gray-400 opacity-80 mb-2">
+              {t("form_description")}
+            </p>
+            <ContactForm />
+          </div>
         </div>
       </div>
-      <div className="w-full bg-green-800 flex justify-center items-center text-white text-sm py-2 lg:text-2xl lg:py-4">
+      <div className="w-full bg-green-800 flex justify-center items-center text-white text-sm py-1 lg:text-md lg:py-2">
         {data.rootLayout?.copyrightStatement}
       </div>
     </footer>
