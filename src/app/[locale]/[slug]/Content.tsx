717946c71276e3a0e06fef60095ba35a615c2e18
nna1205 <ngnhatanh1205.com>
2025-02-20 14:49:06 +0700

    Separate sections renderer to a component used memo() function to optimized  react reconcilation process

---
+++ src/app/[locale]/[slug]/Content.tsx
@@ -0,0 +1,54 @@
+import HeroSection from "@/components/Sections/HeroSectionRecord/HeroSection";
+import TestimonialSection from "@/components/Sections/TestimonialSectionRecord/TestimonialSection";
+import ServiceSection from "@/components/Sections/ServiceSectionRecord/ServiceSection";
+import FeaturedProductSection from "@/components/Sections/FeaturedProductSectionRecord/FeaturedProductSection";
+import { PageQuery, SiteLocale } from "@/graphql/types/graphql";
+import { memo } from "react";
+import { routing } from "@/../i18n/routing";
+import { notFound } from "next/navigation";
+
+const Content = memo(
+  ({ data, locale }: { data: PageQuery; locale: SiteLocale }) => {
+    const sections = data.page?.sections || [];
+
+    if (!sections.length || !routing.locales.includes(locale as SiteLocale)) {
+      notFound();
+    }
+    return (
+      <div className="w-screen min-h-screen py-10 my-20 gap-8 px-4 lg:p-20 font-[family-name:var(--font-geist-sans)]">
+        <main className="flex flex-col gap-40 row-start-2 items-center sm:items-start">
+          {sections.map((section) => {
+            switch (section.__typename) {
+              case "HeroSectionRecord":
+                return (
+                  <HeroSection fragment={section} key={section.__typename} />
+                );
+              case "TestimonialSectionRecord":
+                return (
+                  <TestimonialSection
+                    fragment={section}
+                    key={section.__typename}
+                  />
+                );
+              case "ServicesSectionRecord":
+                return (
+                  <ServiceSection fragment={section} key={section.__typename} />
+                );
+              case "FeaturedProductSectionRecord":
+                return (
+                  <FeaturedProductSection
+                    fragment={section}
+                    key={section.__typename}
+                  />
+                );
+              default:
+                return null;
+            }
+          })}
+        </main>
+      </div>
+    );
+  }
+);
+
+export default Content;
--- src/app/[locale]/[slug]/page.tsx
+++ src/app/[locale]/[slug]/page.tsx
@@ -5,10 +5,7 @@ import {
   type SiteLocale,
 } from "@/graphql/types/graphql";
 import { notFound } from "next/navigation";
-import HeroSection from "@/components/Sections/HeroSectionRecord/HeroSection";
-import TestimonialSection from "@/components/Sections/TestimonialSectionRecord/TestimonialSection";
-import ServiceSection from "@/components/Sections/ServiceSectionRecord/ServiceSection";
-import FeaturedProductSection from "@/components/Sections/FeaturedProductSectionRecord/FeaturedProductSection";
+import Content from "./Content";
 import { toNextMetadata } from "@/utils/SEO";
 import { routing } from "@/../i18n/routing";
 import { setRequestLocale, getTranslations } from "next-intl/server";
@@ -57,46 +54,9 @@ export default async function Page({
   params: Promise<{ slug: string; locale: SiteLocale }>;
 }) {
   const { slug, locale } = await params;
-  const pageData = await request(PageDocument, { slug: slug, locale: locale });
-
-  if (!pageData || !routing.locales.includes(locale as SiteLocale)) {
-    notFound();
-  }
-
   // Enable static rendering
   setRequestLocale(locale);
-  return (
-    <div className="overflow-x-hidden min-h-screen py-10 my-20 gap-8 px-4 lg:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-40 row-start-2 items-center sm:items-start">
-        {pageData.page?.sections.map((section) => {
-          switch (section.__typename) {
-            case "HeroSectionRecord":
-              return (
-                <HeroSection fragment={section} key={section.__typename} />
-              );
-            case "TestimonialSectionRecord":
-              return (
-                <TestimonialSection
-                  fragment={section}
-                  key={section.__typename}
-                />
-              );
-            case "ServicesSectionRecord":
-              return (
-                <ServiceSection fragment={section} key={section.__typename} />
-              );
-            case "FeaturedProductSectionRecord":
-              return (
-                <FeaturedProductSection
-                  fragment={section}
-                  key={section.__typename}
-                />
-              );
-            default:
-              return null;
-          }
-        })}
-      </main>
-    </div>
-  );
+  const pageData = await request(PageDocument, { slug: slug, locale: locale });
+
+  return <Content data={pageData} locale={locale} />;
 }
