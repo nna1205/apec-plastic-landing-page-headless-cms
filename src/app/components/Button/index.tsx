2048eb4e5bd5cbec98bb3227dd4d98ec505db26c
nna1205 <78691537+nna1205@users.noreply.github.com>
2025-02-23 19:23:33 +0700

    Temporally fix accessibility issues of Touch targets do not have sufficient size or spacing.

--- src/app/components/Button/index.tsx
+++ src/app/components/Button/index.tsx
@@ -27,16 +27,12 @@ const Button: React.FC<ButtonProps> = ({
 
   return (
     <button
-      className={`inline-flex items-center justify-center min-w-[48px] min-h-[48px] text-sm text-wrap lg:text-2xl focus:outline-none focus:ring-2 focus:ring-green-400 transition-all ${className}`}
+      className={`inline-flex items-center justify-center min-w-[48px] min-h-[48px] text-sm lg:text-2xl rounded-md focus:outline-none focus:ring-2 focus:ring-green-400 transition-all px-4 py-2 lg:px-6 lg:py-3 ${getVariantClassName(
+        variant
+      )} ${className}`}
       {...rest}
     >
-      <span
-        className={`rounded-md px-4 py-2 lg:px-6 lg:py-3 ${getVariantClassName(
-          variant
-        )}`}
-      >
-        {children}
-      </span>
+      {children}
     </button>
   );
 };
