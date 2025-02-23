918203a394980b85a71f344a9f28260eccd4c2cf
nna1205 <78691537+nna1205@users.noreply.github.com>
2025-02-23 20:21:10 +0700

    Temporaly fix lighthouse accessibility issues

--- src/app/components/Button/index.tsx
+++ src/app/components/Button/index.tsx
@@ -3,35 +3,34 @@ import React from "react";
 export type ButtonProps = {
   variant?: "primary" | "secondary" | "outline"; // Define available variants
   children: React.ReactNode;
+  asChild?: boolean;
   className?: string; // Allow additional class names
 } & React.ButtonHTMLAttributes<HTMLButtonElement>; // Include standard button attributes
 
 const Button: React.FC<ButtonProps> = ({
   variant = "primary",
   children,
+  asChild = false,
   className = "",
   ...rest
 }) => {
-  const getVariantClassName = (variant: ButtonProps["variant"]) => {
-    switch (variant) {
-      case "primary":
-        return "bg-green-800 text-white font-bold";
-      case "secondary":
-        return "bg-green-400 text-white font-bold";
-      case "outline":
-        return "bg-transparent box-border border border-slate-300 text-black font-bold";
-      default:
-        return "";
-    }
+  // Base styles for the button
+  const baseStyles =
+    "min-w-[48px] min-h-[48px] px-4 lg:px-6 py-2 lg:py-3 text-sm lg:text-2xl font-bold rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 transition-colors duration-200";
+
+  // Variant-specific styles
+  const variantStyles = {
+    primary: "bg-green-800 text-white hover:bg-green-700 focus:ring-green-500",
+    secondary: "bg-green-400 text-white hover:bg-gray-700 focus:ring-gray-500",
+    outline:
+      "bg-transparent border border-gray-600 text-gray-700 hover:bg-gray-100 focus:ring-gray-500",
   };
 
+  // Combine base styles, variant styles, and any additional className
+  const combinedClassName = `${baseStyles} ${variantStyles[variant]} ${className}`;
+
   return (
-    <button
-      className={`inline-flex items-center justify-center min-w-[48px] min-h-[48px] text-sm lg:text-2xl rounded-md focus:outline-none focus:ring-2 focus:ring-green-400 transition-all px-4 py-2 lg:px-6 lg:py-3 ${getVariantClassName(
-        variant
-      )} ${className}`}
-      {...rest}
-    >
+    <button className={combinedClassName} {...rest}>
       {children}
     </button>
   );
