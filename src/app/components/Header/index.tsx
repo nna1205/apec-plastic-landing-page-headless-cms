10340c5fd33c4db6e3a8db0be3b09b232546742a
nna1205 <78691537+nna1205@users.noreply.github.com>
2025-02-23 17:51:34 +0700

    Fix link href attributes is not actionable

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -13,7 +13,17 @@ const Header: React.FC<{ data: HeaderProps }> = ({ data }) => {
     <header className="fixed top-0 left-0 w-screen z-50">
       <div className="w-full bg-green-800 flex items-center gap-6 py-3 px-6 lg:px-20 text-white text-sm lg:text-lg">
         {data.brandDetail?.contacts.map((item) => (
-          <a key={item.id} className="flex justify-center items-center">
+          <a
+            key={item.id}
+            className="flex justify-center items-center"
+            href={
+              item.label.toLocaleLowerCase() === "email"
+                ? `mailto:${item.value}`
+                : item.label.toLocaleLowerCase() === "hotline"
+                ? `tel:${item.value.replace(/\s+/g, "")}`
+                : item.value
+            }
+          >
             <Icon
               name={item.icon as IconName}
               className="h-4 w-4 lg:w-6 lg:h-6"
